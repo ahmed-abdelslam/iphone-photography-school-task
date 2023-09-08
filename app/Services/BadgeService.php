a968da2deca4c76bd89de6a514eb15649f6687e0
Ahmed Abdelslam <ahmed.nabolsy6688@gmail.com>
2023-09-08 15:56:21 +0300

    implement checkBadgeUnlock method inside BadgeService, it's used to check if a user will unlock a new badge or not

--- app/Services/BadgeService.php
+++ app/Services/BadgeService.php
@@ -9,6 +9,26 @@ use App\Models\User;
 class BadgeService
 {
     /**
+     * Check if the user has unlocked a new badge or not
+     */
+    public function checkBadgeUnlock(User $user)
+    {
+        // Get the total user's achievements
+        $achievementsCount = $user->achievements()->count();
+
+        // Get the badge that has the target of the same number of achievements
+        $badge = Badge::where('target', $achievementCount)->first();
+
+        // If there is no matching badge, then return and do nothing
+        if (!$badge) {
+            return;
+        }
+
+        // Add the badge to the user
+        $this->addBadge($badge, $user);
+    }
+
+    /**
      * Add a badge for a user
      */
     public function addBadge(Badge $badge, User $user)
