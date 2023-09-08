f1c7aba3bdc01c58208c6b71624025c7fc0ddc27
Ahmed Abdelslam <ahmed.nabolsy6688@gmail.com>
2023-09-08 08:23:24 +0300

    define watched relationship in User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Lesson;
 
 class User extends Authenticatable
 {
@@ -42,4 +43,16 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * Relationships
+     *
+     */
+
+    /**
+      * Get the lessons watched by a user
+      */
+    public function watched() {
+        return $this->belongsToMany(Lesson::class, 'lesson_user')->where('watched', 1);
+    }
 }
